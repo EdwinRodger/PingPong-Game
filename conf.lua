fbd9df3e472f59398bd022e8cf58581b38ee81a3
EdwinRodger <codevaidik@gmail.com>
2023-03-12 23:25:16 +0530

    Initial Commit

---
+++ conf.lua
@@ -0,0 +1,8 @@
+
+function love.conf(t)
+    t.title = "Ping Pong"                  -- The title of the window the game is in (string)
+    t.version = "11.3"                    -- The LÖVE version this game was made for (string)
+    t.console = false                     -- Attach a console (boolean, Windows only)
+    t.window.width = 720                 -- The window width (number)
+    t.window.height = 480                 -- The window height (number)
+end
